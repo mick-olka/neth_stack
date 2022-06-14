ea8d26c6dc19d4a03c92d71efd9bdbf20ff218df
mick-olka <mickolka64@gmail.com>
2022-06-14 20:25:10 +0300

    modules & eslint fixed

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -1,19 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
 import * as homeService from '@/service/home';
 import { catchAsync } from '@/utils/catchAsync';
-
-import { NextFunction, Request, Response } from 'express';
 
 export const getAppInfo = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const result = homeService.getAppInfo();
-
-    res.json(result);
+    res.status(200).json(result);
   },
 );
 
 export const getMainPage = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     //
-    res.render('index', { title: 'Main' });
+    res.render('index', { title: 'Main', arr: [1, 2, 4] });
   },
 );
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,22 @@
 import * as homeController from '@/controller/home';
 
 import { Router } from 'express';
+import * as fs from 'fs';
+import path from 'path';
 
 const router = Router();
 
 router.get('/', homeController.getAppInfo);
 
 router.get('/index', homeController.getMainPage);
+
+//  test/435435/43543 params
+//  test?name=43543&r=ghfjdg
+router.get('/write/', (req, res, next) => {
+  const txt = req.query.text;
+  fs.appendFileSync(path.resolve(__dirname, 'text.txt'), txt as string);
+  let result = fs.readFileSync(path.resolve(__dirname, 'text.txt'), 'utf-8');
+  res.json(result);
+});
 
 export default router;
